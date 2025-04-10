787ae66c67f06b6935c9654f760ed1d2c84f0382
Anjiang Wei <43546559+Anjiang-Wei@users.noreply.github.com>
2025-04-10 15:10:14 -0700

    Create system_install.sh

---
+++ system_install.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential
+sudo apt install -y git curl wget tmux vim nano emacs openssh-server unzip tar gzip bzip2 tree net-tools iproute2 python3 python3-pip htop nmon iftop ffmpeg tmux
+sudo apt-get install -y libopenmpi-dev
